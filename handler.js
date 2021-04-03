d404ed9f62a64e5932fe5bc760c582512cf67988
andré <andredds12@gmail.com>
2021-04-03 20:17:44 -0300

    add serveless framework, update github actions

---
+++ handler.js
@@ -0,0 +1,17 @@
+"use strict";
+
+module.exports.criptoreport = async (event) => {
+  return {
+    statusCode: 200,
+    body: JSON.stringify(
+      {
+        message: "Testing!",
+      },
+      null,
+      2
+    ),
+  };
+
+  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
+  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+};
