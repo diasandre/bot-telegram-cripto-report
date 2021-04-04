f0da8f5e9a42e881997bdeb5dc246e26370b93ad
andré <andredds12@gmail.com>
2021-04-04 01:41:22 -0300

    improve displayed info

--- handler.js
+++ handler.js
@@ -1,7 +1,7 @@
 "use strict";
 
 const axios = require("axios");
-const { format } = require("date-fns");
+const { format, subDays } = require("date-fns");
 
 module.exports.criptoreport = async (event) => {
   const TELEGRAM_TOKEN = process.env.TELEGRAM_TOKEN;
@@ -9,24 +9,40 @@ module.exports.criptoreport = async (event) => {
   const CHAT_ID = process.env.CHAT_ID;
   const CRIPTOS = process.env.CRIPTOS;
 
-  const date = new Date();
+  const actualDate = new Date();
+  const sevenDaysDate = subDays(actualDate, 7);
 
-  const formatedDate = format(date, "yyyy-MM-dd'T'HH:00:00XXX");
+  const apiDateFormatted = format(sevenDaysDate, "yyyy-MM-dd'T'HH:00:00XXX");
+  const API_BASE_URL = `https://api.nomics.com/v1/currencies/sparkline?key=${API_KEY}&ids=${CRIPTOS}&start=${apiDateFormatted}`;
 
-  const API_BASE = `https://api.nomics.com/v1/currencies/sparkline?key=${API_KEY}&ids=${CRIPTOS}&start=${formatedDate}`;
-
-  const { data } = await axios.get(API_BASE);
+  const { data } = await axios.get(API_BASE_URL);
 
   const content = data
-    .map(({ currency, timestamps: [timestamp], prices: [price] }) => {
-      return `<b>${currency}</b> - USD ${price}`;
+    .map(({ currency, prices }) => {
+      const [sevenDaysPrice] = prices;
+      const actualPrice = prices[prices.length - 1];
+
+      const percentual = (
+        ((actualPrice - sevenDaysPrice) / sevenDaysPrice) *
+        100
+      ).toFixed(2);
+
+      const sevenDaysPriceFixed = Number(sevenDaysPrice).toFixed(3);
+      const actualPriceFixed = Number(actualPrice).toFixed(3);
+
+      const hasWarning = percentual < -10;
+
+      const warningEmoji = hasWarning ? "[WARNING]" : "";
+
+      return `<b>${currency}${warningEmoji}</b>\nUSD ${sevenDaysPriceFixed} -> ${percentual}% -> USD ${actualPriceFixed}`;
     })
     .join("\n");
 
-  const response = `<b>Relatório criptomoedas - ${format(
-    date,
-    "dd/MM/yyyy"
-  )}</b>\n \n${content}`;
+  const fromDateFormated = format(sevenDaysDate, "dd/MM/yyyy");
+
+  const toDateFormated = format(actualDate, "dd/MM/yyyy");
+
+  const response = `<b>RELATÓRIO CRIPTOAMIGOS</b>\n<b>${fromDateFormated} -> ${toDateFormated}</b>\n \n${content}`;
 
   const TELEGRAM_API_BASE = `https://api.telegram.org/bot${TELEGRAM_TOKEN}/sendMessage`;
 
