b9e9048910158af6f9508fad124349a3166a0910
andré <andredds12@gmail.com>
2021-04-03 21:45:52 -0300

    implement get api

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # bot-telegram-cripto-report
+
+[![deploy to lambda](https://github.com/diasandre/bot-telegram-cripto-report/actions/workflows/main.yml/badge.svg)](https://github.com/diasandre/bot-telegram-cripto-report/actions/workflows/main.yml)
--- handler.js
+++ handler.js
@@ -1,17 +1,50 @@
 "use strict";
 
-module.exports.criptoreport = async (event) => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: "Testing!",
-      },
-      null,
-      2
-    ),
-  };
+import axios from "axios";
+import { format } from "date-fns";
 
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+module.exports.criptoreport = async (event) => {
+  // const TOKEN_TELEGRAM = process.env.TOKEN_TELEGRAM;
+  const API_KEY = process.env.API_KEY;
+
+  const criptos = ["BTC", "ETH", "BNB", "XVS", "XRP"].join(",");
+
+  const date = new Date();
+
+  const formatedDate = format(date, "YYYY-MM-DDTHH:00:00Z");
+
+  const API_BASE = `https://api.nomics.com/v1/currencies/sparkline?key=${API_KEY}&ids=${criptos}&start=${formatedDate}`;
+
+  axios
+    .get(API_BASE)
+    .then((data) => {
+      const response = data.map(
+        ({ currency, timestamps: [timestamp], prices: [price] }) => {
+          return `${currency} - USD ${price}}`;
+        }
+      );
+
+      return {
+        statusCode: 200,
+        body: JSON.stringify(
+          {
+            message: response.join(","),
+          },
+          null,
+          2
+        ),
+      };
+    })
+    .catch((error) => {
+      return {
+        statusCode: 500,
+        body: JSON.stringify(
+          {
+            message: error,
+          },
+          null,
+          2
+        ),
+      };
+    });
 };
